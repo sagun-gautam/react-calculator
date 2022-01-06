0c1d60c4bdecdb563be54196f342c28c2046f129
Sagun Gautam <gautamsgn@gmail.com>
2022-01-06 23:24:14 +1100

    Initialized repo and created Operator buttons for Caltulator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     	<header className="App-header">
+        	<div className="calculator">
+				<div className="display">
+					<span> (0) </span> 0
+				</div>
+				<div className="operators">
+					<button>/</button>
+					<button>*</button>
+					<button>+</button>
+					<button>-</button>
+					<button>DEL</button>
+				</div>
+
+				<div className="digits">
+
+				<button>0</button>
+				<button>.</button>
+				<button>=</button>
+				</div>
+        	</div>
+      	</header>
     </div>
   );
 }
